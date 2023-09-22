54c79afff2288216ad07ecd02033c40fc97f931e
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2023-09-21 22:36:46 -0300

    cambio datos del proyecto

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'LSE IMPA TRQ'
+copyright = '2023, IMPA TRQ'
+author = 'Fabrizio Carlassara'
 
 release = '0.1'
 version = '0.1.0'
